951ee8718c41247d82c92d03d3aad56511b20371
Koi-AS <lightningblade.901@gmail.com>
2023-04-08 16:06:44 -0400

    Added a new CSS file for the Questions HTML file specifically. Added Pseudocode to the JS file. Added some elements and IDs to the Questions HTML file.

---
+++ assests/js/script.js
@@ -0,0 +1,29 @@
+var startClick = document.querySelector("#start");
+
+//on button click
+startClick.addEventListener("click",function beginQuiz() {
+//start quiz
+//1. change page
+//2. start timer
+//3. display first question
+});
+
+function question() {
+//on question answer
+//1. confirm correct or incorrect
+//  - on question correct highlight green
+//  - on question incorrect highlight red and reduce timer by 10
+//2. change question
+}
+
+//repeat until last question
+//how many questions? 10
+//time penalty? 6s
+
+//on answered last question or timer 0
+//end quiz
+//change page to show results
+//enter initials to save data
+
+//show save data on highscore board
+//show options to return to main menu or play again
