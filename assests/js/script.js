571302af3b3110a8451c7bffb99bed16386c902c
Koi-AS <lightningblade.901@gmail.com>
2023-04-09 23:01:13 -0400

    Put all my questions in an array and started setting up new functions. Renamed 'questions' files to 'leaderboard' in preparation to take the development in a different direction.

--- assests/js/script.js
+++ assests/js/script.js
@@ -8,7 +8,8 @@ var ans3 = document.getElementById("ans3");
 var ans4 = document.getElementById("ans4");
 var responseEl = document.getElementById("response");
 
-//on button click
+var currentQuestionIndex;
+
 function countdown () {
     var timeLeft = 60;
     var timeInterval = setInterval(function() {
@@ -24,126 +25,23 @@ function countdown () {
             endScreen();
         }
     }, 1000)
-}
-
-function beginQuiz() {
-    var responseTime = 5;
-
-    var responseInterval = setInterval(function() {
-        if (responseTime > 1) {
-        }
-    })
-    questionEl.textContent = "Quiz About To Begin!";
-    ans1.textContent = '';
-    ans2.textContent = '';
-    ans3.textContent = '';
-    ans4.textContent = '';
-    var responseInterval = setInterval(function() {
-        if (responseTime >= 1) {
-            responseEl.textContent = "...in " + responseTime;
-            responseTime--;
-        } else {
-            responseEl.textContent = 'Go!';
-            clearInterval(responseInterval);
-        }
-    }, 1000)
-}
-
-beginQuiz();
-countdown ();
-//start quiz
-//1. change page
-//2. start timer
-console.log(countdown);
-//3. display first question
-
-//questions
-//1. How many moons does it take to reach the sun? a: 43109.17
-function question1() {
-    questionEl.textContent = 'Approximately how many moons does it take to reach the sun?';
-    ans1.textContent = '43109.17';
-    ans2.textContent = '49301.71';
-    ans3.textContent = '53109.17';
-    ans4.textContent = '59301.71';
-    if (ans1 === true) {
-        ansBoxEl.addEventListener("click", function() {
-        });
-    }
 };
-question1();
-//2. What color is the sun? a: white
-ansBoxEl.addEventListener("click", function() {
-    questionEl.textContent = 'What color is the sun?';
-    ans1.textContent = 'White';
-    ans2.textContent = 'Green';
-    ans3.textContent = 'Yellow';
-    ans4.textContent = 'Red';
-});
-//3. White is the color commonly attributed to lightning. What popular animated series features a lightning based character? a: Pokemon
-ansBoxEl.addEventListener("click", function() {
-    questionEl.textContent = 'White is the color commonly attributed to lightning. What popular animated series features a lightning based character?';
-    ans1.textContent = 'Avatar: The Last Airbender';
-    ans2.textContent = 'Transformers';
-    ans3.textContent = 'Adventure Time';
-    ans4.textContent = 'Pokemon';
-});
-//4. What is Jung's favorite variable? a: Pikachu
-    ansBoxEl.addEventListener("click", function() {
-    questionEl.textContent = "What is Jung's favorite variable?";
-    ans1.textContent = 'Chicken Nuggies';
-    ans2.textContent = 'Pikachu';
-    ans3.textContent = 'A Students Name';
-    ans4.textContent = 'Mayonnaise';
-});
-//5. How many chicken nuggies does it take to fill a pool if the pool was 10ftx50ftx8ft? a: Approxamitely 23952.1
-ansBoxEl.addEventListener("click", function() {
-    questionEl.textContent = 'How many chicken nuggies does it take to fill a pool if the pool was 10ftx50ftx8ft?';
-    ans1.textContent = '20436.2';
-    ans2.textContent = '23952.1';
-    ans3.textContent = '25064.7';
-    ans4.textContent = '28993.3';
-});
-//6. Water is very healthy. How much have you had today? a: Not enough
-ansBoxEl.addEventListener("click", function() {
-    questionEl.textContent = 'Water is very healthy. How much have you had today?';
-    ans1.textContent = 'None at All';
-    ans2.textContent = 'Not Enough';
-    ans3.textContent = 'Enough';
-    ans4.textContent = 'Plenty';
-});
-//7. What is the main ingredient in Walmart Carrot Cake? a: sugar
-ansBoxEl.addEventListener("click", function() {
-    questionEl.textContent = 'What is the main ingredient in Walmart Carrot Cake?';
-    ans1.textContent = 'Water';
-    ans2.textContent = 'Carrots';
-    ans3.textContent = 'Sugar';
-    ans4.textContent = 'Chicken';
-});
-//8. Make the following sentence make sense: This webpage is getting a(n) ___ as a grade. a: 100%
-ansBoxEl.addEventListener("click", function() {
-    questionEl.textContent = 'Make the following sentence make sense: This webpage is getting a(n) ___ as a grade.';
-    ans1.textContent = '0%';
-    ans2.textContent = '30%';
-    ans3.textContent = '70%';
-    ans4.textContent = '100%';
-});
-//9. Who is the fairest of them all? a: Snow White
-ansBoxEl.addEventListener("click", function() {
-    questionEl.textContent = 'Who is the fairest of them all?';
-    ans1.textContent = 'Cindarella';
-    ans2.textContent = 'Rapunzel';
-    ans3.textContent = 'Snow White';
-    ans4.textContent = '';
-});
-//10. Is this the final question? a: No
-ansBoxEl.addEventListener("click", function() {
-    questionEl.textContent = 'Is this the final question?';
-    ans1.textContent = 'Yes';
-    ans2.textContent = 'No';
-    ans3.textContent = 'Chicken Nuggies';
-    ans4.textContent = 'Maybe';
-});
 
+//on button click
+startClick.addEventListener('click', beginQuiz);
+//start quiz
+function beginQuiz() {
+    //start timer
+    countdown();
+    //display first question
+    nextQuestion();
+};
+
+function nextQuestion() {
+
+};
+
+function selectAnswer () {};
 //on question answer
 //1. confirm correct or incorrect
 //  - on question correct highlight green
@@ -151,7 +49,6 @@ ansBoxEl.addEventListener("click", function() {
 //2. change question
 
 //repeat until last question
-//how many questions? 10
 //time penalty? 6s
 
 //on answered last question or timer 0
@@ -161,3 +58,97 @@ ansBoxEl.addEventListener("click", function() {
 
 //show save data on highscore board
 //show options to return to main menu or play again
+
+//how many questions? 10
+var questions = [
+    {
+        question: 'Approximately how many moons does it take to reach the sun?',
+        answers: [
+            {ans1: '43109.17', correct: true},
+            {ans2: '49301.71', correct: false},
+            {ans3: '53109.17', correct: false},
+            {ans4: '59301.71', correct: false}
+        ]
+    },
+    {
+        question: 'What color is the sun?',
+        answers: [
+            {ans1: 'White', correct: true},
+            {ans2: 'Green', correct: false},
+            {ans3: 'Yellow', correct: false},
+            {ans4: 'Red', correct: false}
+        ]
+    },
+    {
+        question: 'White is the color commonly attributed to lightning. What popular animated series features a lightning based character?',
+        answers: [
+            {ans1: 'Avatar: The Last Airbender', correct: false},
+            {ans2: 'Transformers', correct: false},
+            {ans3: 'Adventure Time', correct: false},
+            {ans4: 'Pokemon', correct: true}
+        ]
+    },
+    {
+        question: "What is Jung's favorite variable?",
+        answers: [
+            {ans1: 'Chicken Nuggies', correct: false},
+            {ans2: 'Pikachu', correct: true},
+            {ans3: 'A Students Name', correct: false},
+            {ans4: 'Mayonnaise', correct: false}
+        ]
+    },
+    {
+        question: 'How many chicken nuggies does it take to fill a pool if the pool was 10ftx50ftx8ft?',
+        answers: [
+            {ans1: '20436.2', correct: false},
+            {ans2: '23952.1', correct: true},
+            {ans3: '25064.7', correct: false},
+            {ans4: '28993.3', correct: false},
+        ]
+    },
+    {
+        question: 'Water is very healthy. How much have you had today?',
+        answers: [
+            {ans1: 'None at All', correct: false},
+            {ans2: 'Not Enough', correct: true},
+            {ans3: 'Enough', correct: false},
+            {ans4: 'Plenty', correct: false},
+        ]
+    },
+    {
+        question: 'What is the main ingredient in Walmart Carrot Cake?',
+        answers: [
+            {ans1: 'Water', correct: false},
+            {ans2: 'Carrots', correct: false},
+            {ans3: 'Sugar', correct: true},
+            {ans4: 'Chicken', correct: false},
+        ]
+    },
+    {
+        question: 'Make the following sentence make sense: This webpage is getting a(n) ___ as a grade.',
+        answers: [
+            {ans1: '0%', correct: false},
+            {ans2: '30%', correct: false},
+            {ans3: '70%', correct: false},
+            {ans4: '100%', correct: true},
+        ]
+    },
+    {
+        question: 'Who is the fairest of them all?',
+        answers: [
+            {ans1: 'Cindarella', correct: false},
+            {ans2: 'Rapunzel', correct: false},
+            {ans3: 'Snow White', correct: true},
+            {ans4: 'The 7 Dwarfs', correct: false},
+        ]
+    },
+    {
+        question: 'Is this the final question?',
+        answers: [
+            {ans1: 'Yes', correct: false},
+            {ans2: 'No', correct: true},
+            {ans3: 'Chicken Nuggies', correct: false},
+            {ans4: 'Maybe', correct: false},
+        ]
+    }
+]
